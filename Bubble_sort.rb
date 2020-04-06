a0b1b94834ea82bc4668b60cb12d1f0b345180e4
imahnama <muannama25@gmail.com>
2020-04-06 18:58:09 +0300

    Create method bubble_sort_by and use yield inside the block

--- Bubble_sort.rb
+++ Bubble_sort.rb
@@ -18,5 +18,26 @@ end
 
 print bubble_sort([4,9,1,6,2])
 
-# def bubble_sort_by(array)
-#   yield
+def bubble_sort_by(array)
+  (array.length).times do
+  swap = false
+
+  (array.length-1).times do |x|
+    if yield(array[x], array[x+1]) < 0
+    array[x] , array[x + 1]  =  array[x] , array[x + 1]
+  elsif yield(array[x], array[x+1]) > 0
+    array[x] , array[x + 1]  = array[x + 1], array[x]
+    swap = true
+  else yield(array[x], array[x+1]) == 0
+    array[x] , array[x+1]  =  array[x] , array[x + 1]
+  end
+end
+break array if not swap
+end
+end
+
+sorted_array = bubble_sort_by(["hi","hello","hey","hola"]) do |left,right|
+  left.length - right.length
+end
+
+puts sorted_array
